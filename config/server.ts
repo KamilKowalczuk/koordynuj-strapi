b92f0febb50fbdb29b55bb269dae9e96b737b1bc
Kamil Kowalczuk <kamil.kowalczuk17@gmail.com>
2025-10-05 16:04:15 +0200

    poprawa strapi

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
+// ścieżka: config/server.ts
+
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '0.0.0.0'), // Upewniamy się, że domyślnie nasłuchuje na wszystkich interfejsach
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
