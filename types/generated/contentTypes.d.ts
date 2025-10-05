45224342ade7430a950fb8e8221e6dda5497bd23
Kamil Kowalczuk <kamil.kowalczuk17@gmail.com>
2025-10-05 19:37:17 +0200

    poprawa strapi

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -431,7 +431,7 @@ export interface ApiBlogPostBlogPost extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    excerpt: Schema.Attribute.String;
+    excerpt: Schema.Attribute.Text;
     featuredImage: Schema.Attribute.Media<
       'images' | 'files' | 'videos' | 'audios'
     >;
