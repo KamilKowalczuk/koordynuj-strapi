6d2ceeb1e875ca044c40f6895eb7f59fc269c537
Kamil Kowalczuk <kamil.kowalczuk17@gmail.com>
2025-10-05 16:52:34 +0200

    poprawa strapi

--- config/server.ts
+++ config/server.ts
@@ -1,12 +1,21 @@
 // config/server.ts
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('URL', 'http://localhost:1337'), // 🔥 TO BYŁO KLUCZOWE!
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+export default ({ env }) => {
+  console.log('🚀 Server Config:');
+  console.log('HOST:', env('HOST', '0.0.0.0'));
+  console.log('PORT:', env.int('PORT', 1337));
+  console.log('URL:', env('URL'));
+  console.log('NODE_ENV:', env('NODE_ENV'));
+  
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: env('URL'),
+    proxy: true,
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+};
